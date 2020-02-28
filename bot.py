f57ef5d9b8a8a69c5cf57c9c6727705280e5469a
Danijel Jurešić <15526078+djuresic@users.noreply.github.com>
2020-02-28 14:36:10 +0100

    Random cats and dogs

--- bot.py
+++ bot.py
@@ -1343,6 +1343,45 @@ async def roll(ctx, low : int, high : int):
     await ctx.send('{0} rolled {1} {2} and got **{3}**.'.format(ctx.message.author.mention, low, high, result))
 
 
+#-------------------- Misc --------------------
+@bot.command()
+async def cat(ctx):
+    """Get a random cat image from The Cat API."""
+    search_url = 'https://api.thecatapi.com/v1/images/search'
+    try:
+        search_headers = {'x-api-key' : CONFIG['CAT']['x-api-key']}
+    except:
+        search_headers = None
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(search_url, headers=search_headers) as resp:
+            json_resp = await resp.json()
+    
+    cat_dict = json_resp[0]
+    cat_img_url = cat_dict['url']
+
+    await ctx.send(f'{cat_img_url}')
+
+
+@bot.command()
+async def dog(ctx):
+    """Get a random dog image from The Dog API."""
+    search_url = 'https://api.thedogapi.com/v1/images/search'
+    try:
+        search_headers = {'x-api-key' : CONFIG['DOG']['x-api-key']}
+    except:
+        search_headers = None
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(search_url, headers=search_headers) as resp:
+            json_resp = await resp.json()
+    
+    dog_dict = json_resp[0]
+    dog_img_url = dog_dict['url']
+
+    await ctx.send(f'{dog_img_url}')
+
+
 #-------------------- Events, Error Handling & Debugging --------------------
 @bot.command()
 @in_whitelist(DISCORD_WHITELIST_IDS)
