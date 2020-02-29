c9d13c80ad483c0e5aba3038f788116d56ef7e83
Danijel Jurešić <15526078+djuresic@users.noreply.github.com>
2020-02-29 14:32:38 +0100

    Moved phpserialize to utils

--- extensions/masterserver.py
+++ extensions/masterserver.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 import aiohttp
 
-import phpserialize
+import utils.phpserialize as phpserialize
 
 import config
 from extensions.checks import is_tester, in_whitelist
