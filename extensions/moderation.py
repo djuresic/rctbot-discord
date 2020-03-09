1e4ad3f4be96f15c373e5713ef6c6af501338d62
Danijel Jurešić <15526078+djuresic@users.noreply.github.com>
2020-03-09 16:37:02 +0100

    So, that was broken

--- extensions/moderation.py
+++ extensions/moderation.py
@@ -20,7 +20,7 @@ class Moderation(commands.Cog):
 
         if offender is not None:
             if offender == ctx.message.author:
-                number += 1 #accounting for command invoking message
+                number += 1 # Accounting for command invoking message
             messages = []
             counter = 0
             async for message_sent in channel.history(limit=250):
@@ -36,7 +36,7 @@ class Moderation(commands.Cog):
 
         else:
             #messages = await channel.history(limit=number).flatten()
-            deleted = await channel.purge(limit=number+1) #accounting for command invoking message
+            deleted = await channel.purge(limit=number+1) # Accounting for command invoking message
             message = await ctx.send('Deleted the last {} messages.'.format(len(deleted)))
             await message.add_reaction('🆗')
             await self.bot.wait_for('reaction_add', check=lambda reaction, user: user == ctx.message.author and reaction.emoji == '🆗' and reaction.message.id == message.id, timeout=30.0)
@@ -48,15 +48,15 @@ class Moderation(commands.Cog):
         if member is None:
             ctx.send("Incorrect format. The correct format is: `.info <user>`")
             return
-        embed = discord.Embed(title="User information", type="rich", color=0xff6600, timestamp=ctx.message.timestamp)
+        embed = discord.Embed(title="Discord Member Information", type="rich", color=0xff6600, timestamp=ctx.message.created_at)
         embed.set_author(name=member.display_name, icon_url=member.avatar_url)
-        embed.add_field(name="Status", value=member.status, inline=True)
-        embed.add_field(name="Currently playing", value=member.game, inline=True)
-        embed.add_field(name="Discord ID", value=member.id, inline=True)
-        embed.add_field(name="Account created at", value=member.created_at, inline=True)
-        embed.add_field(name="Server join date", value=member.joined_at, inline=False)
+        embed.add_field(name="Status:", value=member.status, inline=False)
+        embed.add_field(name="Activities:", value=member.activities, inline=False)
+        embed.add_field(name="Discord ID:", value=member.id, inline=False)
+        embed.add_field(name="Account created at:", value=member.created_at, inline=False)
+        embed.add_field(name="Server join date:", value=member.joined_at, inline=False)
         embed.set_footer(text="RCTBot", icon_url="https://i.imgur.com/Ou1k4lD.png")
-        embed.set_thumbnail(url="https://i.imgur.com/q8KmQtw.png")
+        #embed.set_thumbnail(url="https://i.imgur.com/q8KmQtw.png") # HoN logo
         message = await ctx.send(embed=embed)
         await message.add_reaction('🆗')
         await self.bot.wait_for('reaction_add', check=lambda reaction, user: user == ctx.message.author and reaction.emoji == '🆗' and reaction.message.id == message.id, timeout=30.0)
