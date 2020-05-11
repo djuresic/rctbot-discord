476311a355847b69ceeeb8ea052200be0f859382
Danijel Jurešić <15526078+djuresic@users.noreply.github.com>
2020-05-11 17:42:12 +0200

    Welcome code

--- extensions/welcome.py
+++ extensions/welcome.py
@@ -1,3 +1,5 @@
+import string
+import secrets
 from datetime import datetime, timezone
 
 import discord
@@ -10,6 +12,8 @@ from extensions.checks import is_senior
 class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.code_len = 42
+        self.alphabet = string.ascii_letters + string.digits
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -20,12 +24,20 @@ class Welcome(commands.Cog):
         moderator = discord.utils.get(guild.roles, name="Community Moderator")
         senior = discord.utils.get(guild.roles, name="Senior Tester")
 
+        code = "".join(secrets.choice(self.alphabet) for i in range(self.code_len))
+
+        # This is fucky due to guild, it's fine because only RCT will use it anyway.
+        log_channel = guild.get_channel(config.DISCORD_BOT_LOG_CHANNEL_ID)
+        await log_channel.send(
+            f"[Verification] {member.mention}\n**ID:** {member.id}\n**Code:** {code}"
+        )
+
         embed = discord.Embed(
             title=f"Welcome {member.display_name} to the official Retail Candidate Testers Discord Server!",
             type="rich",
             description=f"""Please tell us your HoN username so that we can set it as your Discord nickname. Be respectful to every player and use common sense. If you have any questions, ask here on {channel.mention} or talk to a {moderator.mention} in private.
             
-            If you have been accepted as a {tester.mention}, please wait for a {senior.mention} to assign you the corresponding role so that you may access our private channels. In case you are not a tester but wish to become one, check the links below for more information.""",
+            If you have been accepted as a {tester.mention}, your verification code is `{code}` and your Discord ID is `{member.id}`. Please proceed as instructed in the forum message and wait for a {senior.mention} to assign you the corresponding role so that you may access our private channels. In case you are not a tester but wish to become one, check the links below for more information.""",
             color=0xFF6600,
             timestamp=datetime.now(timezone.utc),
         )
@@ -58,12 +70,20 @@ class Welcome(commands.Cog):
         moderator = discord.utils.get(guild.roles, name="Community Moderator")
         senior = discord.utils.get(guild.roles, name="Senior Tester")
 
+        code = "".join(secrets.choice(self.alphabet) for i in range(self.code_len))
+
+        # This is fucky due to guild, it's fine because only RCT will use it anyway.
+        log_channel = guild.get_channel(config.DISCORD_BOT_LOG_CHANNEL_ID)
+        await log_channel.send(
+            f"[Verification] {ctx.author.mention}\n**ID:** {ctx.author.id}\n**Code:** {code}"
+        )
+
         embed = discord.Embed(
             title=f"Welcome {ctx.author.display_name} to the official Retail Candidate Testers Discord Server!",
             type="rich",
             description=f"""Please tell us your HoN username so that we can set it as your Discord nickname. Be respectful to every player and use common sense. If you have any questions, ask here on {ctx.channel.mention} or talk to a {moderator.mention} in private.
             
-            If you have been accepted as a {tester.mention}, please wait for a {senior.mention} to assign you the corresponding role so that you may access our private channels. In case you are not a tester but wish to become one, check the links below for more information.""",
+            If you have been accepted as a {tester.mention}, your verification code is `{code}` and your Discord ID is `{ctx.author.id}`. Please proceed as instructed in the forum message and wait for a {senior.mention} to assign you the corresponding role so that you may access our private channels. In case you are not a tester but wish to become one, check the links below for more information.""",
             color=0xFF6600,
             timestamp=datetime.now(timezone.utc),
         )
