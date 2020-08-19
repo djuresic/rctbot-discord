284476d4a825380b74a64aa3db43a11e8d4023a2
djuresic <15526078+djuresic@users.noreply.github.com>
2020-08-20 00:26:41 +0200

    Dunder version before import

--- rctbot/__init__.py
+++ rctbot/__init__.py
@@ -16,10 +16,10 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from rctbot.core.bot import RCTBot
-
-
 __version__ = "0.0.1"
+
+
+from rctbot.core.bot import RCTBot
 
 
 def get_bot():
