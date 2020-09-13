d1ac746606a9ebef4f097bec89845c47c1642bf6
djuresic <15526078+djuresic@users.noreply.github.com>
2020-09-14 01:37:03 +0200

    hero usage dev

--- rctbot/extensions/dev.py
+++ rctbot/extensions/dev.py
@@ -1,4 +1,5 @@
 import asyncio
+import collections
 
 import aiohttp
 import discord
@@ -8,9 +9,17 @@ import rctbot.config
 
 from rctbot.core.mongodb import CLIENT
 from rctbot.core.rct import DatabaseManager, CycleManager, MatchManipulator
-from rctbot.core.checks import is_tester
+from rctbot.core import checks
 from rctbot.hon.acp2 import ACPClient
 from rctbot.hon.masterserver import Client
+from rctbot.hon.utils import hero_name, cli_hero_name
+
+
+def chunks(list_, n):
+    # https://stackoverflow.com/a/312464/13185424
+    """Yield successive n-sized chunks from list."""
+    for i in range(0, len(list_), n):
+        yield list_[i : i + n]
 
 
 class Development(commands.Cog):
@@ -18,6 +27,7 @@ class Development(commands.Cog):
         self.bot = bot
         self.db = CLIENT[rctbot.config.MONGO_DATABASE_NAME]
         self.testers = self.db[rctbot.config.MONGO_TESTING_PLAYERS_COLLECTION_NAME]
+        self.testing_games = self.db[rctbot.config.MONGO_TESTING_GAMES_COLLECTION_NAME]
 
     @commands.command()
     @commands.is_owner()
@@ -88,6 +98,75 @@ class Development(commands.Cog):
     @commands.is_owner()
     async def t5(self, ctx):
         print(rctbot.config.LIST_OF_LISTS)
+
+    # TODO: Move to a different module.
+    @commands.group(aliases=["u"], invoke_without_command=True)
+    @checks.is_senior()
+    async def usage(self, ctx):
+        pipeline = [
+            {"$unwind": "$participants"},
+            {"$project": {"_id": 0, "participants.hero": 1}},
+        ]
+        documents = await (
+            self.testing_games.aggregate(pipeline=pipeline, collation={"locale": "en", "strength": 1})
+        ).to_list(length=None)
+        heroes = [hero_name(document["participants"]["hero"]) for document in documents]
+        counter = collections.Counter(heroes)
+        unique_picks = len(heroes)
+        message = []
+        for hero, times in counter.items():
+            message.append(f"{hero}: **{times}** ({round((times/unique_picks)*100, 2)}%)")
+        message = chunks(sorted(message), 50)
+        for chunk in message:
+            await ctx.send("\n".join(chunk))
+        await ctx.send(f"Total Hero picks: **{unique_picks}**\nDifferent Heroes from Hero Pool: **{len(counter)}**")
+
+    @usage.command(name="hero", aliases=["h"])
+    @checks.is_senior()
+    async def _usage_hero(self, ctx, *hero_name):
+        hero_cli_name = cli_hero_name(" ".join(hero_name))
+        pipeline = [
+            {"$sort": {"participants.nickname": 1}},
+            {"$match": {"participants.hero": hero_cli_name}},
+            {"$unwind": "$participants"},
+            {"$match": {"participants.hero": hero_cli_name}},
+            {"$project": {"_id": 0, "participants.nickname": 1}},
+        ]
+        documents = await (
+            self.testing_games.aggregate(pipeline=pipeline, collation={"locale": "en", "strength": 1})
+        ).to_list(length=None)
+        players = [document["participants"]["nickname"] for document in documents]
+        counter = collections.Counter(players)
+        message = []
+        for player, times in counter.items():
+            message.append(f"{discord.utils.escape_markdown(player)}: **{times}**")
+        message = chunks(sorted(message), 50)
+        for chunk in message:
+            await ctx.send("\n".join(chunk))
+        await ctx.send(f"Times picked: **{len(players)}**\nTimes picked by an unique player: **{len(counter)}**")
+
+    @usage.command(name="player", aliases=["p"])
+    @checks.is_senior()
+    async def _usage_player(self, ctx, player_nickname: str):
+        pipeline = [
+            # {"$sort": {"participants.hero": 1}},
+            {"$match": {"participants.nickname": player_nickname}},
+            {"$unwind": "$participants"},
+            {"$match": {"participants.nickname": player_nickname}},
+            {"$project": {"_id": 0, "participants.hero": 1}},
+        ]
+        documents = await (
+            self.testing_games.aggregate(pipeline=pipeline, collation={"locale": "en", "strength": 1})
+        ).to_list(length=None)
+        heroes = [hero_name(document["participants"]["hero"]) for document in documents]
+        counter = collections.Counter(heroes)
+        message = []
+        for hero, times in counter.items():
+            message.append(f"{hero}: **{times}**")
+        message = chunks(sorted(message), 50)
+        for chunk in message:
+            await ctx.send("\n".join(chunk))
+        await ctx.send(f"Total picks: **{len(heroes)}**\nDifferent Heroes picked: **{len(counter)}**")
 
     @commands.command()
     @commands.is_owner()
